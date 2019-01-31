3f748d766ba92d336191cc040c7da434172e90d3
dave-ck <daveck@protonmail.com>
2019-01-31 22:18:50 +0000

    fully functional \ {removing users, managing tasks}

--- app.js
+++ app.js
@@ -29,50 +29,50 @@ let taskDescriptions = {
         Tag: 'veg',
         Title: 'Vegetables',
         Description: 'Chop peppers, onions and mushrooms as needed. Label with date.',
-        Time: 1,
-        LastDone: "\"2019-01-27T12:00:00.000Z\""
+        Time: 0.8,
+        LastDone: "2019-01-27T12:00:00.000Z"
     },
     coffee: {
         Tag: 'coffee',
         Title: 'Coffee Machine',
         Description: 'Wash the coffee machine and capsule dispenser.',
-        Time: 2,
-        LastDone: "\"2019-01-30T12:00:00.000Z\""
+        Time: 1.8,
+        LastDone: "2019-01-30T12:00:00.000Z"
     },
     mop: {
         Tag: 'mop',
         Title: 'Mop',
         Description: 'Mop the floor. Add one pump from the wall-mounted detergent dispenser to a bucket 1/3rd full of water.',
-        Time: 1,
-        LastDone: "\"2019-01-29T12:00:00.000Z\""
+        Time: 0.8,
+        LastDone: "2019-01-29T12:00:00.000Z"
     },
     probe: {
         Tag: 'probe',
         Title: 'Probe Toasties',
         Description: 'Probe 2 toasties (or reach closing time having made at most 1 toastie).',
-        Time: 1,
-        LastDone: "\"2019-01-30T12:00:00.000Z\""
+        Time: 0.2,
+        LastDone: "2019-01-30T12:00:00.000Z"
     },
     dishes: {
         Tag: 'dishes',
         Title: 'Dishes',
         Description: 'Remove all clean dishes from the drying rack, ensure the cutlery box is closed. Wash dishes from shift with soap and hot water, leave to dry on rack',
-        Time: 1,
-        LastDone: "\"2019-01-30T12:00:00.000Z\""
+        Time: 0.2,
+        LastDone: "2019-01-30T12:00:00.000Z"
     },
     microwave: {
         Tag: 'microwave',
         Title: 'Microwave',
         Description: 'Clean inside microwave with detergent, wipe down with wet sponge. Don\'t forget the top!',
         Time: 4,
-        LastDone: "\"2019-01-23T12:00:00.000Z\""
+        LastDone: "2019-01-23T12:00:00.000Z"
     },
     counter: {
         Tag: 'counter',
         Title: 'Counter',
         Description: 'Clear and wipe down the counters(customer facing and coffee machine)',
         Time: 7,
-        LastDone: "\"2019-01-29T12:00:00.000Z\""
+        LastDone: "2019-01-29T12:00:00.000Z"
     }
 };
 let tasks = ["veg", "coffee", "probe", "dishes", "mop"];
@@ -81,11 +81,10 @@ let evening = ["probe", "dishes", "mop"];
 
 function redact(username) {
     let person = people[username];
-    let redactedPerson = {
+    return {
         surname: person.surname,
         forename: person.forename
     };
-    return redactedPerson;
 
 
 }
@@ -107,7 +106,7 @@ app.get('/auth', function (requ, resp) {
         resp.send(false);
     } else if (people[userName]['password'] === password) {
         //console.log("Verified.");
-        resp.send(true);
+        resp.send(people[userName].access_token);
     } else {
         //console.log("Phony password/uname pair");
         resp.send(false);
@@ -121,14 +120,13 @@ app.get('/afternoon', function (request, response) {
     });
     let responseJSON = JSON.stringify(afternoonTasks);
     response.send(responseJSON);
-    console.log(responseJSON);
 });
 
 app.get('/evening', function (request, response) {
     let eveningTasks = [];
     tasks.forEach(function (taskName) {
         if (evening.includes(taskName)) eveningTasks.push(taskDescriptions[taskName]);
-    })
+    });
     let responseJSON = JSON.stringify(eveningTasks);
     response.send(responseJSON);
     console.log(responseJSON);
@@ -139,23 +137,52 @@ app.get('/people/:username', function (request, response) {
     response.send(redact(username));
 });
 
-app.get('/people/', function (request, response) {
-    let username = request.params.username;
-    // strip out passwords and emails - sensitive data
-    let redactedPeople = {};
-    Object.keys(people).map(function (username) {
-        redactedPeople[username] = redact(username);
-    });
-    response.send(redactedPeople);
+app.get('/people/', function (requ, resp) {
+    console.log(requ.query.access_token);
+    if (admin_tokens.includes(requ.query.access_token)) {
+        resp.send(people);
+    } else {
+        // strip out passwords and emails - sensitive data
+        let redactedPeople = {};
+        Object.keys(people).map(function (username) {
+            redactedPeople[username] = redact(username);
+        });
+        resp.send(redactedPeople);
+    }
 });
 
-// bodge, not fully implemented
 app.post('/people', function (requ, resp) {
+    console.log("People POST called");
+    console.log(requ.body);
+    console.log(requ);
+    console.log(requ.body.access_token);
     if (admin_tokens.includes(requ.body.access_token)) {
-        // process the request, reject with 400 if duplicate ID.
-        resp.send(400);
+        let forename = requ.body.forename;
+        let surname = requ.body.surname;
+        let username = requ.body.username;
+        let email = requ.body.email;
+        let password = requ.body.password;
+        if (forename && surname && username && email && password) {
+            if (Object.keys(people).includes(username)) {
+                //duplicate username not allowed
+                resp.sendStatus(400);
+                console.log("Rejected duplicate username");
+
+            } else {
+                people[username] = {
+                    forename: forename,
+                    surname: surname,
+                    email: email,
+                    password: password
+                };
+                console.log("Successfully added person:" + people[username]);
+                resp.sendStatus(200);
+            }
+        }
+    } else {
+        console.log("invalid auth token");
+        resp.sendStatus(403);
     }
-    resp.send(403);
 });
 
 app.post('/done', function (requ, resp) {
@@ -165,8 +192,15 @@ app.post('/done', function (requ, resp) {
         if (taskTag in taskDescriptions) {
             taskDescriptions[taskTag].LastDone = new Date();
             resp.send(200);
-        } else resp.send(400);
-    } else resp.send(403);
+            console.log("Accepted task as done.")
+        } else {
+            console.log("Invalid tag received");
+            resp.send(400);
+        }
+    } else {
+        console.log("Unauthorized auth token");
+        resp.send(403);
+    }
 });
 
 module.exports = app;
--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -2,6 +2,13 @@ let loggedInAs = null;
 let access_token = "";
 $('#failedLoginText').hide();
 $('#failedAddPeople').hide();
+$('.loggedIn').hide();
+
+// thanks to Nico Tejera at https://stackoverflow.com/questions/1714786/query-string-encoding-of-a-javascript-object
+// returns something like "access_token=concertina&username=bobthebuilder"
+function serialise(obj) {
+    return Object.keys(obj).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(obj[k])}`).join('&');
+}
 
 function updateNames(names) {
     let nameList = "<ul>";
@@ -17,14 +24,21 @@ function afternoon() {
     $.get("./afternoon", function (jsonTaskList) {
         let taskList = JSON.parse(jsonTaskList);
         console.log(taskList);
-        updateTasks($('#afternoonPills'), $('#afternoonContents'), taskList);
+        updateTasks($('#afternoonPills'), $('#afternoonContents'), taskList, "afternoon");
+    });
+}
+
+function evening() {
+    $.get("./evening", function (jsonTaskList) {
+        let taskList = JSON.parse(jsonTaskList);
+        updateTasks($('#eveningPills'), $('#eveningContents'), taskList, "evening");
     });
 }
 
 function refreshLoggedIn(uName) {
     // log in button
     let text = " Log in";
-    let classes = "glyphicon glyphicon-log-in"
+    let classes = "glyphicon glyphicon-log-in";
     if (uName != null) {
         text = " " + loggedInAs;
         classes = "glyphicon glyphicon-user";
@@ -32,45 +46,87 @@ function refreshLoggedIn(uName) {
     $('#loginLink').html(`<span class="${classes}"></span> ${text}`);
 }
 
-function updateTasks(pillDiv, contentDiv, taskList) {
+function updateTasks(pillDiv, contentDiv, taskList, afteve) {
     let pillHTML = "";
     let contentHTML = "";
     let listLen = taskList.length;
-    console.log(taskList);
     console.log(taskList[0]);
+    let now = new Date();
     for (i = 0; i < listLen; i++) {
         let task = taskList[i];
         let taskName = task.Title;
-        let pill = `<li><a id="afternoonPill${i}" data-toggle=\"tab\" href=\"#afternoon${i}\"> Item: ${taskName} </a></li>`;
-        let taskDiv = `<h4>${taskName}</h4><p>${task.Description}</p>`;
-        let content = `<div id=\"afternoon${i}\" class=\"tab-pane fade\">${taskDiv}</div>`;
+        let pill = `<li><a id="${afteve}Pill${i}" data-toggle=\"tab\" href=\"#${afteve}${i}\"> Item: ${taskName} </a></li>`;
+        let lastDone = new Date(task.LastDone);
+        let timeDelta = now - lastDone;
+        let daysDelta = timeDelta / (1000 * 60 * 60 * 24);
+        let btnStyle = "btn-success";
+        let urgencyCaption = "Task has been done.";
+        if (daysDelta > 0.2) {
+            btnStyle = "btn-warning";
+            urgencyCaption = "Not yet done this shift."
+        }
+        if (daysDelta > task.Time) {
+            btnStyle = "btn-danger";
+            urgencyCaption = "Task must be done this shift."
+        }
+        let taskDiv = `<h4>${taskName}</h4><p>${task.Description}</p><p>${urgencyCaption}</p><button id="${afteve + i}Done" type="button" class="btn ${btnStyle}">Done!</button>`;
+        let content = `<div id=\"${afteve}${i}\" class=\"tab-pane fade\">${taskDiv}</div>`;
         pillHTML += pill;
         contentHTML += content;
-        console.log("Content generated.")
     }
     pillDiv.html(pillHTML);
     contentDiv.html(contentHTML);
     for (i = 0; i < listLen; i++) {
-        $(`#afternoonPill${i}`).on('click', function () {
-            console.log("Attempting to load dynamic content.");
+        let task = taskList[i];
+        $(`#${afteve + i}Done`).on('click', function () {
+            let button = this;
+            $.post("./done", serialise({
+                access_token: access_token,
+                task: task.Tag
+            }), function (status) {
+                console.log(typeof this);
+                console.log(this);
+                button.classList.add("btn-success");
+                button.classList.remove("btn-warning", "btn-danger");
+                console.log("Task accepted as done:" + status);
+            });
+
         });
-        console.log("Pill linked: " + i);
+        console.log("Button linked: " + i);
     }
-    console.log("Content loaded.")
+}
+
+
+function peopleTable(people) {
+    let tableHTML = "";
+    Object.keys(people).forEach(function (username) {
+        let forename = people[username].forename;
+        let surname = people[username].surname;
+        let email = people[username].email;
+        let password = people[username].password;
+        tableHTML += `<tr><td>${username}</td><td>${forename}</td><td>${surname}</td><td>${email}</td><td>${password}</td></tr>`;
+    });
+    $('#userTableBody').html(tableHTML);
 }
 
 
 $('#afternoonLink').on('click', afternoon);
+$('#eveningLink').on('click', evening);
 
 // logging in
 $('#loginForm').on('submit', function (formOut) {
     formOut.preventDefault();
-    let uname = this.elements[0].value;
-    let pwd = this.elements[1].value;
-    $.get("./auth", "userName=" + uname + "&password=" + pwd,
+    $('.loggedOut').hide();
+    $('.loggedIn').show();
+
+    let username = this.elements[0].value;
+    let password = this.elements[1].value;
+    $.get("./auth", "userName=" + username + "&password=" + password,
         function (authenticated) {
             if (authenticated) {
-                loggedInAs = uname;
+                loggedInAs = username;
+                access_token = authenticated;
+                console.log("Access token granted: " + access_token);
                 $('#failedLoginText').hide();
             } else {
                 console.log("Failed to log in");
@@ -78,5 +134,42 @@ $('#loginForm').on('submit', function (formOut) {
             }
             refreshLoggedIn(loggedInAs);
         });
+    $('#loginLink').removeAttribute("aria-expanded");
     return false;
+});
+
+$('#createAccount').on('submit', function (formOut) {
+    formOut.preventDefault();
+    let username = this.elements[0].value;
+    let password = this.elements[1].value;
+    let confirmPassword = this.elements[2].value;
+    if (password !== confirmPassword) return false;
+    let forename = this.elements[3].value;
+    let surname = this.elements[4].value;
+    let email = this.elements[5].value;
+    console.log(serialise({
+        access_token: access_token,
+        username: username,
+        forename: forename,
+        surname: surname,
+        email: email,
+        password: password
+    }));
+    $.post("./people", serialise({
+        access_token: access_token,
+        username: username,
+        forename: forename,
+        surname: surname,
+        email: email,
+        password: password
+    }), function (status) {
+        console.log("Add person status:" + status);
+    });
+});
+
+$('#removeAccountPill').on('click', function () {
+    $.get("./people", serialise({access_token: access_token}), function (people) {
+        console.log(people);
+        peopleTable(people);
+    });
 });
